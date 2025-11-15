e4e4c9776e678eb98610823194e8eb21908b3d58
Russell Gillmore <regillmore@alaska.edu>
2025-11-14 19:48:44 -0900

    grey horizon

--- src/game/initGame.js
+++ src/game/initGame.js
@@ -1001,7 +1001,7 @@ export async function initializeGame({
         colorAttachments: [
           {
             view: textureView,
-            clearValue: { r: 0.0, g: 0.0, b: 0.0, a: 1.0 },
+            clearValue: { r: 0.05, g: 0.06, b: 0.08, a: 1.0 },
             loadOp: 'clear',
             storeOp: 'store'
           }
